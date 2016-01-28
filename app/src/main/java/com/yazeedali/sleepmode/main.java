b2517f8e593badcfe8165f35133544b989ef616a
noody63 <noody63@users.noreply.github.com>
2016-01-28 14:15:49 +0300

    Update main.java

--- app/src/main/java/com/yazeedali/sleepmode/main.java
+++ app/src/main/java/com/yazeedali/sleepmode/main.java
@@ -2,12 +2,15 @@ package com.yazeedali.sleepmode;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothManager;
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
 import android.media.AudioManager;
+import android.net.Uri;
 import android.net.wifi.WifiManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,10 +18,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.Button;
 
 
 public class main extends AppCompatActivity {
+    Window window;
+
+    ContentResolver cResolver;
+    int brightness;
 
 
     @Override
@@ -26,25 +34,34 @@ public class main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-final Button button1 =(Button) findViewById(R.id.button1);
+        setSupportActionBar(toolbar);
+
+        final Button button1 =(Button) findViewById(R.id.button1);
         final WifiManager wifiManager = (WifiManager)this.getSystemService(Context.WIFI_SERVICE);
         final BluetoothAdapter bluetoothAdapter;
         final AudioManager audio = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+
+
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR2) {
             BluetoothManager bluetoothManager = (BluetoothManager) this.getSystemService(Context.BLUETOOTH_SERVICE);
             bluetoothAdapter = bluetoothManager.getAdapter();
         }else
             bluetoothAdapter=BluetoothAdapter.getDefaultAdapter();
 
-        setSupportActionBar(toolbar);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 wifiManager.setWifiEnabled(false);
                 bluetoothAdapter.disable();
-
                 audio.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                turnGPSOff();
+
+
+
+
+
+
 
 
             }
@@ -54,33 +71,47 @@ final Button button1 =(Button) findViewById(R.id.button1);
 
 
 
-
          );
 
+        }
+    private void turnGPSOff(){
+        String provider = Settings.Secure.getString(getContentResolver(), Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
 
+        if(provider.contains("gps")){
+            final Intent poke = new Intent();
+            poke.setClassName("com.android.settings", "com.android.settings.widget.SettingsAppWidgetProvider");
+            poke.addCategory(Intent.CATEGORY_ALTERNATIVE);
+            poke.setData(Uri.parse("3"));
+            sendBroadcast(poke);
+        }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent i=new Intent(this, info.class);
-            startActivity(i);
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.menu, menu);
             return true;
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                Intent i=new Intent(this, info.class);
+                startActivity(i);
+                return true;
+
+            }
+
+            return super.onOptionsItemSelected(item);
 
         }
+    }
 
         return super.onOptionsItemSelected(item);
 
